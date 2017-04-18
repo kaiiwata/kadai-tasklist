717fcd24a6b22d38bbf69ad9b538b91ae501b462
kaiiwata <n_k_iwata@yahoo.co.jp>
2017-04-18 00:56:05 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,8 +47,8 @@ class TasksController < ApplicationController
       redirect_to tasks_url
   end
   
-  def message_params
-    params.require(:message).permit(:content)
+  def task_params
+    params.require(:task).permit(:content)
   end
   
 end
